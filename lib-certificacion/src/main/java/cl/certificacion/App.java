04a677d2d9fab0c13e41d02c6b26b88ab01da05b
AZURIAN\cgarcia <cgarcia@azurian.com>
2017-06-20 17:23:00 -0400

    Creación librería con maven.

---
+++ lib-certificacion/src/main/java/cl/certificacion/App.java
@@ -0,0 +1,13 @@
+package cl.certificacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
