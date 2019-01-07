5d92e502822b3cd1e847c69f30ae44864b371136
Unknown <garcia.quintupil@gmail.com>
2019-01-07 14:26:35 -0300

    Hola Mundo desde comentario, ignorar archivos eclipse

--- lib-certificacion/src/main/java/cl/certificacion/junit/App.java
+++ lib-certificacion/src/main/java/cl/certificacion/junit/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        //Comentario de prueba
+        
+    	// \u000d System.out.println("Hello World!");
+    	System.out.println("comentado");
     }
 }
