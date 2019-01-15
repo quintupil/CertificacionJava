6bd5b5524ec9ae892506b10fdfd151a6f76e4dff
Unknown <garcia.quintupil@gmail.com>
2019-01-15 17:23:21 -0300

    Capitulo Clases y Objetos
    Se crea clase Intervalo.

---
+++ lib-escuelaIT/src/main/java/escuelait/poo/intervalo/Intervalo.java
@@ -0,0 +1,87 @@
+package escuelait.poo.intervalo;
+
+public class Intervalo {
+	
+	public Intervalo( double inferior, double superior ) {
+		
+	}
+	
+	public Intervalo( double superior ) {
+		
+	}
+	
+	public Intervalo( Intervalo intervalo ) {
+		
+	}
+	
+	public Intervalo() {
+		
+	}
+	
+	public Intervalo clone() {
+		return null;
+	}
+	
+	public double longitud() {
+		return 0;
+	}
+	
+	public void desplazar( double desplazamiento ) {
+		
+	}
+	
+	public Intervalo desplazado( double desplazamiento ) {
+		return null;
+	}
+	
+	public boolean incluye( double valor ) {
+		return false;
+	}
+	
+	public boolean incluye( Intervalo intervalo ) {
+		return false;
+	}
+	
+	public boolean equals( Intervalo intervalo ) {
+		return false;
+	}
+	
+	public Intervalo interseccion( Intervalo intervalo ) {
+		return null;
+	}
+	
+	public boolean intersecta( Intervalo intervalo ) {
+		return false;
+	}
+	
+	public void oponer() {
+		
+	}
+	
+	public void doblar() {
+		
+	}
+	
+	public void recoger() {
+		
+	}
+	
+	public void mostrar() {
+		
+	}
+	
+	public Intervalo[] trocear( int trozos ) {
+		return null;
+	}
+}
+
+class Coordenada {
+	
+	public void recoger() {
+		
+	}
+	
+	public void mostarr() {
+		
+	}
+}
